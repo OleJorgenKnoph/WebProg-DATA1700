d12ba634e6af6c1e32f0cc9fcf8a2afbebb0ee8c
Ole Jørgen Knoph <s354371@oslomet.no>
2021-02-19 21:16:12 +0100

    l

---
+++ Intro_klient_tjener/src/test/java/oslo_met/webprog/WebprogApplicationTests.java
@@ -0,0 +1,13 @@
+package oslo_met.webprog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebprogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
