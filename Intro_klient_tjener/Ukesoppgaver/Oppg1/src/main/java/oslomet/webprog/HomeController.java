9f71ff76b2497e850b78630159906f1d5dc78f12
Ole Jørgen Knoph <s354371@oslomet.no>
2021-02-23 14:50:55 +0100

    Uke 6 oppg 2 ferdig

--- Intro_klient_tjener/Ukesoppgaver/Oppg1/src/main/java/oslomet/webprog/HomeController.java
+++ Intro_klient_tjener/Ukesoppgaver/Oppg1/src/main/java/oslomet/webprog/HomeController.java
@@ -1,8 +1,56 @@
 package oslomet.webprog;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final Integer[] tempArray = new Integer[]{-3, -2, 2, 7, 12, 16, 18, 17, 12, 7, 3, -2};
+
+    @GetMapping("/snittTemp")
+    public int snittTemp(String maaned) {
+
+        int temp = 0;
+
+        switch (maaned) {
+            case "JANUAR":
+                temp = tempArray[0];
+                break;
+            case "FEBRUAR":
+                temp = tempArray[1];
+                break;
+            case "MARS":
+                temp = tempArray[2];
+                break;
+            case "APRIL":
+                temp = tempArray[3];
+                break;
+            case "MAI":
+                temp = tempArray[4];
+                break;
+            case "JUNI":
+                temp = tempArray[5];
+                break;
+            case "JULI":
+                temp = tempArray[6];
+                break;
+            case "AUGUST":
+                temp = tempArray[7];
+                break;
+            case "SEPTEMBER":
+                temp = tempArray[8];
+                break;
+            case "OKTOBER":
+                temp = tempArray[9];
+                break;
+            case "NOVEMBER":
+                temp = tempArray[10];
+                break;
+            case "DESEMBER":
+                temp = tempArray[11];
+                break;
+        }
+        return temp;
+    }
 }
---
+++ Intro_klient_tjener/Ukesoppgaver/Oppg2/src/main/java/oslomet/webprog/Valuta.java
@@ -0,0 +1,30 @@
+package oslomet.webprog;
+
+public class Valuta {
+    private String name;
+    private Double value;
+
+    public Valuta(String name, Double value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Valuta(){
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
